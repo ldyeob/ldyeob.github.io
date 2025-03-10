e5390f710606bc12c2e1423d42fcaaf26cb0dfc3
ldyeob <13932ldy@gmail.com>
2025-03-10 19:36:21 +0900

    Update hw01_2021147610.js

--- hw01/hw01_2021147610.js
+++ hw01/hw01_2021147610.js
@@ -17,23 +17,27 @@ render();
 // Render loop
 function render() {
     gl.enable(gl.SCISSOR_TEST);
+    gl.viewport(0, 0, canvas.width/2, canvas.height/2);
     gl.scissor(0, 0, canvas.width/2, canvas.height/2);
     gl.clearColor(0.0, 0.0, 1.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);  
 
+    gl.viewport(canvas.width/2, 0, canvas.width/2, canvas.height/2);
     gl.scissor(canvas.width/2, 0, canvas.width/2, canvas.height/2);
     gl.clearColor(1.0, 1.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);  
 
+    gl.viewport(0, canvas.height/2, canvas.width/2, canvas.height/2);
     gl.scissor(0, canvas.height/2, canvas.width/2, canvas.height/2);
     gl.clearColor(1.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);  
 
+    gl.viewport(canvas.width/2, canvas.height/2, canvas.width/2, canvas.height/2);
     gl.scissor(canvas.width/2, canvas.height/2, canvas.width/2, canvas.height/2);
     gl.clearColor(0.0, 1.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);  
 
-    gl.disable(gl.SCISSOR_TEST);
+    // gl.disable(gl.SCISSOR_TEST);
 }
 
 // Resize viewport when window size changes
