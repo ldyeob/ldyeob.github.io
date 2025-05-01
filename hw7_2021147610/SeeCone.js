f38fba7ae0ac7d57e49f3894541ed1acb6cd5201
ldyeob <13932ldy@gmail.com>
2025-05-02 00:38:30 +0900

    Update SeeCone.js

--- hw7_2021147610/SeeCone.js
+++ hw7_2021147610/SeeCone.js
@@ -14,7 +14,7 @@ import { resizeAspectRatio, setupText, updateText} from '../util/util.js';
 import { Shader, readShaderFile } from '../util/shader.js';
 import { Cube } from '../util/cube.js';
 import { Arcball } from '../util/arcball.js';
-import { Cone } from '../cone.js';
+import { Cone } from './cone.js';
 
 
 const canvas = document.getElementById('glCanvas');
